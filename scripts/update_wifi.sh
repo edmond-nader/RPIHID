27ecd1ec0a4cff2271903dbce7f94977f900213f
edmond-nader <49365047+edmond-nader@users.noreply.github.com>
2025-03-11 17:23:12 +0200

    Update update_wifi.sh

--- scripts/update_wifi.sh
+++ scripts/update_wifi.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # update_wifi.sh
-# This script updates the wpa_supplicant configuration with a new network block.
-# It expects two parameters: SSID and password.
-# It filters out the plain-text password (commented out) so only the hashed PSK is appended.
-
+# Updates the wpa_supplicant configuration with a new network block,
+# filtering out the plain text password.
 set -e
 set -u
 
@@ -15,8 +13,6 @@ fi
 SSID="$1"
 PASSWORD="$2"
 
-# Generate the network block and remove the commented plain-text password.
+# Generate network block and remove the commented plain text password.
 NETWORK_CONFIG=$(wpa_passphrase "$SSID" "$PASSWORD" | sed '/^#psk=/d')
-
-# Append the network block to /etc/wpa_supplicant/wpa_supplicant.conf.
 echo "$NETWORK_CONFIG" >> /etc/wpa_supplicant/wpa_supplicant.conf

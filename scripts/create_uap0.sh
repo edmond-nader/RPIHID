396cf6b2a10562756ee9dec6c9119ef9b484eb3d
edmond-nader <49365047+edmond-nader@users.noreply.github.com>
2025-03-13 11:42:48 +0200

    Update create_uap0.sh

--- scripts/create_uap0.sh
+++ scripts/create_uap0.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # create_uap0.sh
 # Creates a virtual AP interface (uap0) on wlan0 if it doesn't exist, then brings it up.
-
+# This script sets PATH to include /usr/sbin and /sbin so that 'iw' is found.
 set -e
+
+export PATH=$PATH:/usr/sbin:/sbin
 
 if ip link show uap0 >/dev/null 2>&1; then
     echo "Interface uap0 already exists."
